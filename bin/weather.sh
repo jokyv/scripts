17c4e928966dd5cb41b4c95298e78debd360469e
John Kyvetos <johnkyvetos@gmail.com>
2025-01-01 18:30:06 +0800

    feat(weather): sample code for wttr.in

---
+++ bin/weather.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# for more info: https://github.com/chubin/wttr.in
+# function weather in linux_util.py as well
+
+curl wttr.in/Singapore?format=1
+curl wttr.in/Singapore?format=2
+curl wttr.in/Singapore?format=3
+curl wttr.in/Singapore?format=4
+curl wttr.in/Serangoon
+
+
+curl -s 'wttr.in/{London,Athens,Singapore,Shanghai}?format=3'
